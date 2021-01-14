8ff74ea8fe01b611064f9615c270cbb362be9739
Dzyfhuba <hafidz21ub@gmail.com>
2021-01-14 18:08:32 +0700

    checkpoint

---
+++ Assets/Scripts/Gameplay.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Gameplay : MonoBehaviour
+{
+ public void MainMenu ()
+   {
+       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+   }   
+}
---
+++ Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+  
+
+    void Start() {
+        	StartCoroutine (ToMainMenu ());
+    }
+
+    IEnumerator ToMainMenu(){
+    	yield return new WaitForSeconds(5);
+       	SceneManager.LoadScene(1);
+    }
+
+}
