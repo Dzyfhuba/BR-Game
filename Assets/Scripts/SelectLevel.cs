6fa6b3e8079e41689ab6eeb8acfa280d04c5b04f
wh4td0y0um34n <agunggumilang716@gmail.com>
2021-02-20 13:26:44 +0700

    penambahan SelectLevel

---
+++ Assets/Scripts/SelectLevel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SelectLevel : MonoBehaviour
+{
+    public void Level1()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void Level2()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void Level3()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void Level4()
+    {
+        SceneManager.LoadScene(6);
+    }
+
+  
+}
